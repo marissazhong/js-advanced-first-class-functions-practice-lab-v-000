9cb803f8a8769609d2b5c4208c6bfd7e85075726
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 19:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function logDriverNames(driver) {
+  driver.forEach(function(element) {console.log(element.name)});
+}
+
+function logDriversByHometown(driver, location) {
+  driver.forEach(function(element) {
+    if (element.hometown === location) {
+      console.log(element.name);
+    }});
+}
+
+function driversByRevenue(driver) {
+
+}
+
+function driversByName(driver) {
+
+}
+
+function totalRevenue(driver) {
+
+}
+
+function averageRevenue(driver) {
+
+}
