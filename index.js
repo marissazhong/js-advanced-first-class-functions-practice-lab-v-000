3b8023cc0dd1362f6aac95fdfc0724e9ba37afb5
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 20:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,11 @@ function totalRevenue(driver) {
 function averageRevenue(driver) {
 
 }
+
+const drivers = [
+  { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
+  { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
+  { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
+  { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
+  { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
+];
