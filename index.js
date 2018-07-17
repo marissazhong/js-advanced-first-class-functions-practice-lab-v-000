64133d5de2c8e6b7776ab52d2e5a25d7b276c021
Marissa Zhong <mwz2101@columbia.edu>
2018-07-17 19:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logDriversByHometown(driver, location) {
 }
 
 function driversByRevenue(driver) {
-  let sorted_driver = driver;
+  let sorted_driver = driver.slice(0);
   sorted_driver.sort(function(a, b) {
     return a.revenue - b.revenue;
   })
